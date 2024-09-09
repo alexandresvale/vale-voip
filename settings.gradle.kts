195c1fb5f9985b86ff71ed7785a3dd75c83ff2e5
Alexandre Vale <alexandr3.silv4@live.com>
2024-09-09 01:33:05 -0300

    Adicionado maven linphone em settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,14 +5,24 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io")  }
+        maven {
+            name = "linphone.org maven repository"
+            url = uri("https://linphone.org/maven_repository/")
+            content {
+                includeGroup("org.linphone")
+            }
+        }
     }
 }
 
 rootProject.name = "Vale VoIP"
 include(":app")
 include(":telecom")
+include(":pjsua2")
