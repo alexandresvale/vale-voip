150ed8c6228e0e27480d0030b563b8f46488edda
Alexandre Vale <alexandr3.silv4@live.com>
2024-09-17 23:59:37 -0300

    Implementado caso de uso para adicionar conta no sqlite

---
+++ app/src/main/java/com/example/valevoip/domain/usecase/InsertConfigUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.valevoip.domain.usecase
+
+import com.example.valevoip.data.AccountRepository
+import com.example.valevoip.domain.model.AccountModel
+
+class InsertConfigUseCase(
+    private val accountRepository: AccountRepository
+) {
+    suspend operator fun invoke(accountModel: AccountModel) = accountRepository.insertAccount(accountModel)
+}
