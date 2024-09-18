9147ed734c768e44a8d8f22efd54c3910612769c
Alexandre Vale <alexandr3.silv4@live.com>
2024-09-18 00:14:23 -0300

    Estrutura inicial de broadcast

---
+++ app/src/main/java/com/example/valevoip/IncomingCallReceiver.kt
@@ -0,0 +1,16 @@
+package com.example.valevoip
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.sip.SipAudioCall
+import com.example.valevoip.presentation.main.presentation.MainActivity
+
+
+class IncomingCallReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        var incomingCall: SipAudioCall? = null
+        val activity = context as? MainActivity
+
+    }
+}
