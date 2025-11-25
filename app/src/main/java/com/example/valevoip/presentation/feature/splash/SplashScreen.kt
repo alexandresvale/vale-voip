73b5350303067f7c454967f4bd4350144dc73611
Alexandre Vale <alexandr3.silv4@live.com>
2025-11-24 22:35:53 -0300

    Adicionado SplashScreen na feature

---
+++ app/src/main/java/com/example/valevoip/presentation/feature/splash/SplashScreen.kt
@@ -0,0 +1,35 @@
+package com.example.valevoip.presentation.feature.splash
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.valevoip.presentation.feature.main.MainViewModel
+
+@Composable
+fun SplashScreen(
+    onNavigate: (hasConfig: Boolean) -> Unit,
+    viewModel: MainViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(key1 = true) {
+        viewModel.checkUseConfig()
+    }
+
+    uiState.hasUserConfig?.let {
+        onNavigate(it)
+    }
+
+    SplashLayout()
+}
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen(
+        onNavigate = { }
+    )
+}
